5b454e0866e18237fa55944b41945a653fe0911e
Nikkolas Jackson <NikkolasJ@outlook.com>
2025-04-06 01:02:29 -0400

    fix: Toast now works properly

--- PassGuard/gui/components/toast.py
+++ PassGuard/gui/components/toast.py
@@ -1,24 +1,31 @@
 from customtkinter import CTkToplevel, CTkLabel
 
 class Toast(CTkToplevel):
-    def __init__(self, parent, message, duration):
+    def __init__(self, parent, message, duration=2000):
         super().__init__(parent)
-        self.geometry("240x50")
-        self.overrideredirect(True)
-        self.configure(fg_color="FF6666", corner_radius=20)
         self.parent = parent
         self.message = message
         self.duration = duration
+        self.setup_ui()
+        self.update_position()
+        self.show_toast()
+
         self.bind_id = self.parent.bind("<Configure>", self.on_parent_move)
 
-    def display_toast_components(self):
-        label = CTkLabel(
+    def setup_ui(self):
+        WIN_WIDTH = 240
+        WIN_HEIGHT = 50
+
+        self.geometry(f"{WIN_WIDTH}x{WIN_HEIGHT}")
+        self.overrideredirect(True)
+        self.configure(fg_color="#FF6666", corner_radius=20)
+        self.label = CTkLabel(
             self,
             text=self.message,
             text_color="white",
             font=("Segoe UI", 14)
         )
-        label.pack(expand=True, fill="both", padx=10, pady=10)
+        self.label.pack(expand=True, fill="both", padx=10, pady=5)
 
     def update_position(self):
         parent_x = self.parent.winfo_x()
@@ -28,8 +35,8 @@ class Toast(CTkToplevel):
         y = parent_y + 50
         self.geometry(f"+{x}+{y}")
 
-    def on_parent_move(self, event):
-       self.update_position()
+    def on_parent_move(self, event=None):
+        self.update_position()
 
     def destroy_toast(self):
         self.parent.unbind("<Configure>", self.bind_id)
